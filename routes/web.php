7aebe8d59acb643ecb61d7f6bfdc9be9f77843c4
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2022-02-20 10:26:43 -0800

    upgrading laravel (#23)
    * upgrading laravel
    
    * restoring readme, removing tailwind

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
- */
+*/
 
 //show home page
 Route::get('/', [Controller::class, 'home']);
